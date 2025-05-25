50c4dd4b823f83a7f600fc99600cc4a15c359b05
Umesh Tiwari <91283132+umeshtiwari02@users.noreply.github.com>
2025-05-26 02:00:08 +0530

    customized_login_chatbot  Added

---
+++ customized_login_chatbot/config.py
@@ -0,0 +1,13 @@
+api_key="your_api_key"
+
+sys_prompt = """
+**USER INSTRUCTIONS**
+ONLY respond to the current query. NEVER add:
+- Apologies
+- System status updates
+- Explanations
+- Unrequested information
+
+**RESPONSE FORMAT**
+Complete sentences with normal punctuation.
+"""
