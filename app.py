12802f89b7e1698b824028c622248bb82b26505f
Umesh Tiwari <91283132+umeshtiwari02@users.noreply.github.com>
2025-06-05 01:04:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,2 @@
-from groq import Groq
-from config import api_key  # import from config.py
-
-client = Groq(api_key=api_key)
-
-def main():
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {"role": "user", "content": "Explain the importance of low latency LLMs"}
-        ],
-        model="llama3-8b-8192",
-    )
-
-    print(chat_completion.choices[0].message.content)
-
-if __name__ == "__main__":
-    main()
+print("Hello World")
+print("I am testing whether my code is deploying it into the server or not");
